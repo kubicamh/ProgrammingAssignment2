aecfd5289578cfee87e561be73eab6cc6b50e5d7
Michael Kubica <kubicamh@hotmail.com>
2015-02-15 10:46:43 -0500

    Updates 2-15-15

--- cachematrix.R
+++ cachematrix.R
@@ -2,11 +2,20 @@
 ## Assignment 2 of R Programing through Coursera/John Hopkins
 ## Instructors: Roger D. Peng, PhD, Jeff Leek, PhD, Brian Caffo, PhD
 
-
 ## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
@@ -17,4 +26,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
 }
